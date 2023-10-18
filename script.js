e7e1ef6b06290c96de334caecdc63a5034471b56
mutasim chaudhary <mutasim.chaud@gmail.com>
2023-10-18 20:31:50 +0100

    promts user to choose length and displays it

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  var lengthPassword = prompt("How long do you want your password to be (8-128 characters)" , "length");
+  return lengthPassword;
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
