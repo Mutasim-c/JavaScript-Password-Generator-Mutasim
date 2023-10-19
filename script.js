cdec883b422511a750f66d5d0d4f5533c853df93
mutasim chaudhary <mutasim.chaud@gmail.com>
2023-10-19 15:57:53 +0100

    added all the prompts and most of the arrays

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var lowerCaseCharacters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperCaseCharacters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var numberList = ["1","2","3","4","5","6","7","8","9","0"]
+
+var specialCharacters = [" ","!","#","$","%","&","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~"];
+// " ' \
 function generatePassword() {
-  var lengthPassword = prompt("How long do you want your password to be (8-128 characters)" , "length");
+  var lengthPassword = prompt("How long do you want your password to be (8-128 characters)?" , "length");
+  var lowerCase = confirm("Do you want lower case characters in you password?");
+  var upperCase = confirm("Do you want upper case characters in you password?");
+  var numeric = confirm("Do you want your password to contain numbers?");
+  var specialChar = confirm("Do you want you password to contain special characters?");
+  console.log(lowerCase);
+  console.log(upperCase);
+  lowerCase = confirm("Do you want lower case characters in you password?");
+  console.log(lowerCase);
   return lengthPassword;
 }
 
